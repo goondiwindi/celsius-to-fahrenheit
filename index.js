52fd8e78cdeb244bbd9721b7e304ff8c92d66e30
Pavel Nikolaev <pnikolaev859@gmail.com>
2020-01-18 11:44:46 +0300

    first local commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const celsius = document.getElementById('celsius');
+const fahrenheit = document.getElementById('fahrenheit');
+
+const setBothFromC = value => {
+  celsius.value = value;
+  fahrenheit.value = (32 + (9 / 5) * celsius.value).toFixed(1);
+};
+
+const setBothFromF = value => {
+  fahrenheit.value = value;
+  celsius.value = ((5 / 9) * (fahrenheit.value - 32)).toFixed(1);
+};
+
+celsius.addEventListener('input', e => setBothFromC(e.target.value));
+fahrenheit.addEventListener('input', e => setBothFromF(e.target.value));
